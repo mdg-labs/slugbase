cfdd01f5319d2dcc03f59d72b5e6f4af4c29f838
mdguggenbichler <michael@mdg-labs.dev>
2026-05-31 02:53:16 +0000

    feat(crypto)[SB-8]: add AES-GCM CRYPTO interface with sample setting

--- packages/backend/src/crypto/crypto.module.ts
+++ packages/backend/src/crypto/crypto.module.ts
@@ -1,4 +1,22 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 
-@Module({})
+import { ConfigModule } from "../config/config.module.js";
+import { DbModule } from "../db/db.module.js";
+import { AesGcmCryptoService } from "./aes-gcm-crypto.service.js";
+import { CRYPTO } from "./crypto.tokens.js";
+import { SampleEncryptedSettingService } from "./sample-encrypted-setting.service.js";
+
+@Global()
+@Module({
+  imports: [ConfigModule, DbModule],
+  providers: [
+    AesGcmCryptoService,
+    {
+      provide: CRYPTO,
+      useExisting: AesGcmCryptoService,
+    },
+    SampleEncryptedSettingService,
+  ],
+  exports: [CRYPTO, AesGcmCryptoService, SampleEncryptedSettingService],
+})
 export class CryptoModule {}
